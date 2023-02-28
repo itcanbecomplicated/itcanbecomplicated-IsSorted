32b5e1c5112a74a56b6847178f8821a354c29606
Nikita <itcanbecomplicatedinfo@gmail.com>
2023-02-28 04:56:12 +0000

    w5d5c2

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class IsSorted {
     /**
      * Return true if arr is sorted, smallest to largest, false otherwise.
@@ -7,6 +9,14 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        int[] arr2 = arr.clone();
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != arr2[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
